112ed4eba77d9c14b3af90cbf8b743b9c0024ab8
Oliver Welter <oliver.welter@gmx.net>
2015-12-07 01:31:59 +0100

    Adds RTHumidity.h and RTPressure.h

--- rtimulib/src/main/java/org/bytedeco/javacpp/presets/rtimulib.java
+++ rtimulib/src/main/java/org/bytedeco/javacpp/presets/rtimulib.java
@@ -4,7 +4,7 @@ import org.bytedeco.javacpp.annotation.*;
 import org.bytedeco.javacpp.tools.*;
 
 @Properties(target = "org.bytedeco.javacpp.rtimulib", value = {
-    @Platform(include = {"RTMath.h", "RTIMUHal.h", "RTIMULibDefs.h", "RTIMUSettings.h", "IMUDrivers/RTIMU.h"},
+    @Platform(include = {"RTMath.h", "RTIMUHal.h", "RTIMULibDefs.h", "RTIMUSettings.h", "IMUDrivers/RTIMU.h", "IMUDrivers/RTHumidity.h", "IMUDrivers/RTPressure.h"},
         link = "RTIMULib")})
 public class rtimulib implements InfoMapper {
   public void map(InfoMap infoMap) {
